7fd667afd7dc946b2360efc978506eace4bbb561
AlanYoung <2306915539@qq.com>
2020-08-14 16:15:18 +0800

    插入排序

--- sort/insertionSort.go
+++ sort/insertionSort.go
@@ -1,1 +1,18 @@
 package sort
+
+func (data *DataArr) InsertionSort()  {
+	len := len(*data)
+
+	// 取一个数向前边的子数组中插入
+	// 插完之后前边的数是有序状态， 跟冒泡差不多
+	// 不同的是冒泡是做相邻比较每轮得出一个最值 插入是向有序的数组中插入 插完还是有序的
+	// 只不过是有数组存储的特性 插入的位置需要依次挪动 所以看起来像冒泡
+	for i := 1; i < len; i++ {
+		for j := i; j > 0; j-- {
+			if (*data)[j] < (*data)[j-1] {
+				(*data)[j], (*data)[j-1] = (*data)[j-1], (*data)[j]
+			}
+		}
+	}
+	data.Print()
+}
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -15,3 +15,12 @@ func TestDataArr_BubbleSort(t *testing.T) {
 	s.Print()
 	t.Log("ok")
 }
+
+func TestDataArr_InsertionSort(t *testing.T) {
+	var s DataArr = []int{4, 5, 100, 3, 20, 40, 50}
+	s.InsertionSort()
+	s.Print()
+	t.Log("ok")
+}
+
+
