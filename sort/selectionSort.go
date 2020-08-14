aa91b4aa0ad48d8349b3cd762fe9646561fa9df7
AlanYoung <2306915539@qq.com>
2020-08-14 10:48:29 +0800

    各排序的时间复杂度表

--- sort/selectionSort.go
+++ sort/selectionSort.go
@@ -1,10 +1,11 @@
 package sort
 
 // 选择排序
+// 每轮寻找最小值的下标 将最小值交换到最前面 接着处理后边的
 func (data *DataArr) SelectionSort() {
 	len := len(*data)
 
-	for i := 0; i < len - 1; i++ {
+	for i := 0; i < len-1; i++ {
 		minIndex := i
 
 		for j := i + 1; j < len; j++ {
