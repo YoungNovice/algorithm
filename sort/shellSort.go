6a35e7dd02ec0e2008cb516fb1ae1f3c9c585cd4
AlanYoung <2306915539@qq.com>
2020-08-14 16:29:32 +0800

    shell排序

--- sort/shellSort.go
+++ sort/shellSort.go
@@ -1,9 +1,15 @@
 package sort
 
 // 希尔排序 取间隔排序
-func (data *DataArr) ShellSort()  {
+func (data *DataArr) ShellSort() {
 	len := len(*data)
-	for gap := len >> 1; gap > 0; gap /= 2 {
+
+	// Knuth序列
+	h := 1
+	for h <= len/3 {
+		h = h*3 + 1
+	}
+	for gap := h; gap > 0; gap = (gap - 1) / 3 {
 		for i := gap; i < len; i++ {
 			for j := i; j > gap-1; j -= gap {
 				if (*data)[j] < (*data)[j-1] {
