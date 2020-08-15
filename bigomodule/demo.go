b59f9494b6e28093b18d4566ae7eff22cea41309
AlanYoung <2306915539@qq.com>
2020-08-15 11:32:46 +0800

    各种时间复杂度的大致模型

---
+++ bigomodule/demo.go
@@ -0,0 +1,51 @@
+package bigomodule
+
+// O(1)
+func o1() {
+	x := 0
+	y := 0
+	temp := x
+	x = y
+	y = temp
+}
+
+// O(n)
+func On() {
+	n := 100
+	x := 0
+	for i := 1; i <= n; i++ {
+		x++
+	}
+}
+
+// O(logN)
+func LogN() {
+	i := 1
+	n := 100
+	for i < n {
+		i = i * 2
+	}
+}
+
+// O(nlogN)
+func OnlogN() {
+	n := 100
+	for i := 1; i <= n; i++ {
+		x := 1
+		for x < n {
+			x = x*2
+		}
+	}
+}
+
+// O(n^2)
+func ON2() {
+	n := 100
+	x := 1
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= n; j++ {
+			x++
+		}
+	}
+}
+
