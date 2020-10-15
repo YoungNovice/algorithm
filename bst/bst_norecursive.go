3a8404bd8098fcde656bd181976f4dbbe816bf45
AlanYoung <2306915539@qq.com>
2020-10-15 14:52:37 +0800

    二分搜索树中序遍历非递归

--- bst/bst_norecursive.go
+++ bst/bst_norecursive.go
@@ -4,55 +4,58 @@ package bst
 // 非递归1
 // (先放右孩子 再放左孩子) 出栈时机是左孩子先出栈 所以先右后左
 // 同时由于先序遍历是先访问根的所以看着比较简单
+func (b *BST) PreOrderNR0(f func(*node)) {
+	var s []*node
+	s = append(s, b.root)
+
+	for len(s) > 0 {
+		// pop
+		top := s[len(s)-1]
+		f(top)
+		s = s[:len(s)-1]
+		// push right if exists
+		if top.right != nil {
+			s = append(s, top.right)
+		}
+		// push left if exists
+		if top.left != nil {
+			s = append(s, top.left)
+		}
+	}
+}
+
 func (b *BST) PreOrderNR1(f func(*node)) {
-	var s []*node
-	s = append(s, b.root)
-
-	for len(s) > 0 {
-		// pop
-		top := s[len(s)-1]
-		f(top)
-		s = s[:len(s)-1]
-		// push right if exists
-		if top.right != nil {
-			s = append(s, top.right)
-		}
-		// push left if exists
-		if top.left != nil {
-			s = append(s, top.left)
-		}
-	}
-}
-
-func (b *BST) PreOrderNR2(f func(*node)) {
-	var s []*node
-	s = append(s, b.root)
-
-	for len(s) > 0 {
-		// pop
-		top := s[len(s)-1]
-		f(top)
-		s = s[:len(s)-1]
-		// push right if exists
-		if top.right != nil {
-			s = append(s, top.right)
-		}
-		// push left if exists
-		if top.left != nil {
-			s = append(s, top.left)
-		}
-	}
-}
-
-// 中序遍历非递归 一直向左遍历到底 底部出栈 出栈后访问结点，之后转到右子树
-func (b *BST) InOrderNR(f func(*node)) {
 	if b.root == nil {
 		return
 	}
 
 	var s []*node
 	cur := b.root
-	for len(s) > 0 || cur != nil {
+	for cur != nil || len(s) > 0 {
+		for cur != nil {
+			f(cur)
+			// push
+			s = append(s, cur)
+			cur = cur.left
+		}
+		if len(s) > 0 {
+			// pop
+			cur = s[len(s)-1]
+			s = s[:len(s)-1]
+			cur = cur.right
+		}
+	}
+}
+
+// 中序遍历非递归 一直向左遍历到底 底部出栈 出栈后访问结点，之后转到右子树
+func (b *BST) InOrderNR1(f func(*node)) {
+	if b.root == nil {
+		return
+	}
+
+	var s []*node
+	cur := b.root
+	for cur != nil || len(s) > 0 {
 		// 当前结点有左孩子一路push
 		for cur != nil {
 			s = append(s, cur)
@@ -70,23 +73,40 @@ func (b *BST) InOrderNR(f func(*node)) {
 	}
 }
 
-func (b *BST) InOrderNR1(f func(*node)) {
+
+// 后续遍历比较麻烦 需要一个结点需要入两次栈
+func (b *BST) PostOrderNR1(f func(*node)) {
 	if b.root == nil {
 		return
 	}
-	var s []*node
+	type PostNode struct {
+		n *node
+		flag int
+	}
+	var s []*PostNode
 	cur := b.root
 
 	for len(s) > 0 || cur != nil {
 		if cur != nil {
-			s = append(s, cur)
+			// 第一次push
+			s = append(s, &PostNode{cur, 1})
 			cur = cur.left
 		} else {
 			// pop
-			cur = s[len(s)-1]
+			top:= s[len(s)-1]
 			s = s[:len(s)-1]
-			f(cur)
-			cur = cur.right
+			cur = top.n
+			if top.flag == 1 {
+				top.flag = 2
+				// 第二次push
+				s = append(s, top)
+				// 转向右子树
+				cur = cur.right
+			} else {
+				// flag == 2
+				f(cur)
+				cur = nil
+			}
 		}
 	}
 }
---
+++ bst/bst_norecursive_test.go
@@ -0,0 +1,28 @@
+package bst
+
+import "testing"
+
+func TestBST_PostOrderNR1(t *testing.T) {
+	type fields struct {
+		root *node
+		size int
+	}
+	type args struct {
+		f func(*node)
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := &BST{
+				root: tt.fields.root,
+				size: tt.fields.size,
+			}
+		})
+	}
+}
